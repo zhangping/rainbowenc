f93a56532a06c229722d5fcde97866bf28dffb8f
Zhang Ping <dqzhangp@163.com>
2012-03-04 19:19:22 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,36 @@
 from distutils.core import setup
+import os
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+rainbowenc_dir = 'rainbowenc'
+
+for dirpath, dirnames, filenames in os.walk(rainbowenc_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
 # Dynamically calculate the version based on rainbowenc.VERSION.
 version = __import__('rainbowenc').get_version()
@@ -11,10 +43,8 @@ setup(
     author_email = 'dqzhangp@163.com',
     description = 'A real time encoder based on gstreamer',
     #download_url = '',
-    packages = ['rainbowenc', 'rainbowenc.templates', 'rainbowenc.static'],
-    package_data = {
-        'rainbowenc.templates': ['rainbowenc/templates/*'],
-        'rainbowenc.static': ['rainbowenc/static/*']},
+    packages = packages,
+    data_files = data_files,
     scripts=['rainbowenc/rainbowenc.py'],
     classifiers = [
         'Development Status :: Under development',
