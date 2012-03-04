142d288d56e06291f6686d08a6194c1447c76c3e
Zhang Ping <dqzhangp@163.com>
2012-03-04 23:34:31 +0800

    setuptools

--- setup.py
+++ setup.py
@@ -1,44 +1,4 @@
-from distutils.core import setup
-from distutils.command.install_data import install_data
-from distutils.command.install import INSTALL_SCHEMES
-import os
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-rainbowenc_dir = 'rainbowenc'
-
-for dirpath, dirnames, filenames in os.walk(rainbowenc_dir):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+from setuptools import setup, find_packages
 
 # Dynamically calculate the version based on rainbowenc.VERSION.
 version = __import__('rainbowenc').get_version()
@@ -50,10 +10,11 @@ setup(
     author = 'Zhang Ping',
     author_email = 'dqzhangp@163.com',
     description = 'A real time encoder based on gstreamer',
-    #download_url = '',
-    packages = packages,
-    cmdclass = {'install_data': install_data},
-    data_files = data_files,
+    packages = find_packages (),
+    install_requires = [
+        'v4l2',],
+    data_files = [['rainbowenc/static', ['rainbowenc/static/jquery.js']],
+        ['rainbowenc/templates', ['rainbowenc/templates/login.html']],],
     scripts=['rainbowenc/rainbowenc.py'],
     classifiers = [
         'Development Status :: Under development',
