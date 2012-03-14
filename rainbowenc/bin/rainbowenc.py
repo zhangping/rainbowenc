71a782f351c5a8f84afe00e33a4a05c55ce1c29d
zhangping <zhangping@zhangping-Lenovo.(none)>
2012-03-14 17:16:57 +0800

    add move rainbowenc.py to bin/, add core/daemon.py

---
+++ rainbowenc/bin/rainbowenc.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from rainbowenc import daemon
+
+class rainbowenc (daemon):
+        def run (self):
+                while True:
+                        time.sleep (1)
+
+if __name__ == "__main__":
+        service = rainbowenc ('/var/run/rainbow.pid')
+        if len(sys.argv) == 2:
+                if 'start' == sys.argv[1]:
+                        sys.argv[1] = '8080'
+                        service.start()
+                elif 'stop' == sys.argv[1]:
+                        service.stop()
+                elif 'restart' == sys.argv[1]:
+                        service.restart()
+                else:
+                        print "Unknown command"
+                        sys.exit(2)
+                sys.exit(0)
+        else:
+                print "usage: %s start|stop|restart" % sys.argv[0]
+                sys.exit(2)
+
