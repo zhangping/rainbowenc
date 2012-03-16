1523d44877a476af76bcc0b58d89103da741ac24
Zhang Ping <dqzhangp@163.com>
2012-03-16 10:41:26 +0800

    format

--- setup.py
+++ setup.py
@@ -4,26 +4,26 @@ from setuptools import setup, find_packages
 version = __import__('rainbowenc').get_version()
 
 setup(
-    name = "Rainbowenc",
-    version = version,
-    url = 'http://www.rainbowenc.org/',
-    author = 'Zhang Ping',
-    author_email = 'dqzhangp@163.com',
-    description = 'A real time encoder based on gstreamer',
-    packages = find_packages (),
-    install_requires = [
-        'v4l2',],
-    data_files = [['rainbowenc/static', ['rainbowenc/static/login.html']],],
-    scripts=['rainbowenc/bin/rainbow.py'],
-    classifiers = [
-        'Development Status :: Under development',
-        'Intended Audience :: Users',
-        'License :: GPL License',
-        'Operating System :: Linux',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.5',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-   ],
+        name = "Rainbowenc",
+        version = version,
+        url = 'http://www.rainbowenc.org/',
+        author = 'Zhang Ping',
+        author_email = 'dqzhangp@163.com',
+        description = 'A real time encoder based on gstreamer',
+        packages = find_packages (),
+        install_requires = [
+                'web.py',],
+        data_files = [['rainbowenc/static', ['rainbowenc/static/login.html']],],
+        scripts=['rainbowenc/bin/rainbow.py'],
+        classifiers = [
+                'Development Status :: Under development',
+                'Intended Audience :: Users',
+                'License :: GPL License',
+                'Operating System :: Linux',
+                'Programming Language :: Python',
+                'Programming Language :: Python :: 2.5',
+                'Programming Language :: Python :: 2.6',
+                'Programming Language :: Python :: 2.7',
+        ],
 )
 
