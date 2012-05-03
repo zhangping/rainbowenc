b5a10dcf910b1d536d1a2cc07a97622ac4acf33b
dqzhangp@163.com <zhangping@rainbow.(none)>
2012-05-03 15:49:33 +0800

    reduce latency

--- rainbowenc/recorderctl.py
+++ rainbowenc/recorderctl.py
@@ -12,7 +12,7 @@ rainbowpre = (
 "! udpsink host=%s port=%s "
 "v4l2src hue=-2147483647 contrast=-2147483647 brightness=-2147483647 saturation=-2147483647 ! queue "
 "! videoscale ! video/x-raw-yuv,width=720,height=576 ! queue "
-"! x264enc byte-stream=true threads=0 key-int-max=50 bitrate=1200 bframes=1 qp-min=1 qp-max=51 qp-step=50 vbv-buf-capacity=300 threads=4 ! queue "
+"! x264enc byte-stream=true threads=0 key-int-max=50 bitrate=1200 bframes=1 qp-min=1 qp-max=51 qp-step=50 vbv-buf-capacity=300 threads=4 tune=zerolatency ! queue "
 "! muxer. "
 "s. ! queue ! udpsink host=127.0.0.1 port=60001"
 )
